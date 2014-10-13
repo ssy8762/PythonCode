961615de919edc30aa00459d8a43ca3708441252
ssy8762 <ssy8762@gmail.com>
2014-10-13 14:45:44 +0800

    Chapter1_2_1text

---
+++ Chapter1_2_1text.py
@@ -0,0 +1,13 @@
+x = int(input("Please enter an integer:"))
+
+
+if x <= 100:
+    x < 50
+    print("small")
+elif x > 50:
+    print("large")
+elif x == 50:
+    print("half")
+else:
+    print("wrong")
+    
